74ff7ac319195ae5182441ec981da3f51e5889e5
warriorsyn <mjcgamer32@gmail.com>
2019-05-05 16:14:12 -0300

    SellerController

---
+++ Controllers/SellersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWeb.Models;
+using SalesWeb.Services;
+
+namespace SalesWeb.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerService;
+
+        public SellersController(SellerService sellerService)
+        {
+            _sellerService = sellerService;
+        } 
+
+        public IActionResult Index()
+        {
+            
+            return View(_sellerService.FindAll());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Services/SellerService.cs
@@ -0,0 +1,29 @@
+﻿using SalesWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWeb.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebContext _context;
+
+        public SellerService(SalesWebContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+        
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
+    }
+}
